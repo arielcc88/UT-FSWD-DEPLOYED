4b3fbfe81d1cac1a3eabc40bd93e830caf9ea37d
Ariel Cuesta <arielcc88@gmail.com>
2020-05-16 00:03:13 -0500

    welcome page text input validation complete. Only validates for field not empty.

--- code_quiz_js/assets/js/main.js
+++ code_quiz_js/assets/js/main.js
@@ -1,6 +1,73 @@
-$(document).ready({
-//TODO: prevent default behavior of submit button
-//TODO: capture user's name
+//TODO: prevent default behavior of submit button of welcome form
+//TODO: define user object
+	//>> validate input not empty
+	//>> create user object
+		//>> full name
+		//>> score
+		//>> correct
+		//>> wrong
+		//>> missed
+	//>> json.stringify
+	//>> store object (client side storage)
 //TODO: 
 
+//************************************************************************************
+
+/*
+------------
+VARIABLE DECLARATIONS
+------------
+*/
+const welcomeForm = document.querySelector("#welcome-form");
+const startBtn = document.querySelector("#start-button");
+const welcomeInput = document.querySelector("#welcome-input");
+const alertNotify = document.querySelector("#alert-notify");
+
+
+
+/*
+------------
+EVENT LISTENERS
+------------
+*/
+//preventing button submit action on form
+startBtn.addEventListener("click", function(e){
+	e.preventDefault();
+	//validate user input function
+	if(fnIsEmpty()){
+		console.log("not empt");
+	}
+	else {
+		console.log("empt");
+		//change alert div class: display block.
+		alertNotify.classList.remove("alert-no-visible");
+	}
+	//create user object
+
+	//call startGame function
 });
+
+//Welcome page text input listener
+welcomeInput.addEventListener("keyup", function(){
+	//hiding alert notification in case is shown
+	alertNotify.classList.add("alert-no-visible");
+})
+
+
+
+
+/*
+------------
+FUNCTION DECLARATIONS
+------------
+*/
+//Validating input --> not empty
+function fnIsEmpty(){
+	const userNameInput = welcomeInput.value;
+	if(userNameInput){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
