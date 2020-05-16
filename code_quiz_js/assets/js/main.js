b125b0a32fff05f0d7f36b5566422fac316d42c1
Ariel Cuesta <arielcc88@gmail.com>
2020-05-16 15:46:06 -0500

    user object created and store on welcome page after input validation.

--- code_quiz_js/assets/js/main.js
+++ code_quiz_js/assets/js/main.js
@@ -1,15 +1,4 @@
-//TODO: --done-- prevent default behavior of submit button of welcome form 
-//TODO: define user object
-	//>> --done-- validate input not empty
-	//>> create user object
-		//>> full name
-		//>> score
-		//>> correct
-		//>> wrong
-		//>> missed
-	//>> json.stringify
-	//>> store object (client side storage)
-//TODO: 
+//TODO:
 
 //************************************************************************************
 
@@ -23,39 +12,34 @@ const startBtn = document.querySelector("#start-button");
 const welcomeInput = document.querySelector("#welcome-input");
 const alertNotify = document.querySelector("#alert-notify");
 
-
-
 /*
 ------------
 EVENT LISTENERS
 ------------
 */
 //preventing button submit action on form
-startBtn.addEventListener("click", function(e){
-	e.preventDefault();
-	//validate user input function
-	if(fnIsEmpty()){
-		console.log("not empt");
-		//call user object creation & storage
-	}
-	else {
-		console.log("empt");
-		//change alert div class: display block.
-		alertNotify.classList.remove("alert-no-visible");
-	}
-	//create user object
+startBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  //validate user input function
+  if (fnIsEmpty()) {
+    console.log("not empt");
+    //call user object creation & storage
+    fnSetObjInLocal(fnUserObject(welcomeInput.value));
+  } else {
+    console.log("empt");
+    //change alert div class: display block.
+    alertNotify.classList.remove("alert-no-visible");
+  }
+  //create user object
 
-	//call startGame function
+  //call startGame function
 });
 
 //Welcome page text input listener
-welcomeInput.addEventListener("keyup", function(){
-	//hiding alert notification in case is shown
-	alertNotify.classList.add("alert-no-visible");
-})
-
-
-
+welcomeInput.addEventListener("keyup", function () {
+  //hiding alert notification in case is shown
+  alertNotify.classList.add("alert-no-visible");
+});
 
 /*
 ------------
@@ -63,31 +47,39 @@ FUNCTION DECLARATIONS
 ------------
 */
 //Validating input --> not empty
-function fnIsEmpty(){
-	if(welcomeInput.value){
-		return true;
-	}
-	else{
-		return false;
-	}
+function fnIsEmpty() {
+  if (welcomeInput.value) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Function returns user object
-function fnUserObject(userName){
-	return {
-		username: userName,
-		score: 0,
-		correct: 0,
-		wrong: 0,
-		missed: 0
-	}
+function fnUserObject(userName) {
+  return {
+    username: userName,
+    score: 0,
+    correct: 0,
+    wrong: 0,
+  };
 }
 
-
-// function stores string in local storage
-function fnStoreInLocal(data){
-	
+//store the object in the local storage
+function fnSetObjInLocal(objName) {
+  localStorage.setItem(objName, JSON.stringify(objName));
+  //TODO: validate if user name exist - show message
 }
 
-
-//function converts object, arrays into strings
+// function updates values in local storage
+function fnUpdateValueInLocal(objName, objKey, objValue) {
+  //getting existing data if any
+  let storedObj = localStorage.getItem(objName);
+  //if no existing object, create one.
+  //otherwise, retrieve existing data
+  storedObj = storedObj ? JSON.parse(storedObj) : {};
+  //set new key-value
+  storedObj[objKey] = objValue;
+  //store back the object in the local storage
+  localStorage.setItem(objName, JSON.stringify(storedObj));
+}
