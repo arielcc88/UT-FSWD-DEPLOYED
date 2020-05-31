884365ea4ce40a827d56c9c49a0970629b530193
Ariel Cuesta <arielcc88@gmail.com>
2020-05-30 22:40:02 -0500

    completed functions to query API, get lat and lon and put one call API End Point URL together with data from previous API call.

--- weather_dashboard/assets/js/main.js
+++ weather_dashboard/assets/js/main.js
@@ -16,51 +16,55 @@ $(document).ready(function(){
         event.preventDefault();
         //calling main function to query API end-point
         //TODO: call main function
+        fnGetOneCallAPIData($("#ct-search-input").val());
     });
 
     /**** FUNCTIONS ****/
-    function fnGetLastStoredCity(){
-
+    function fnGetOneCallAPIData(ctName){
+        //function returns Lon and Lat for a city
+        let regEndPntURL = fnGetEndPntURL(ctName);
+        //pass end point URL into the API content retriever function
+        fnQueryWeatherEndPnt(regEndPntURL);
     }
 
-    function fnGetWeatherData(ctName){
-        //1. validate ctName is not empty
-        if (ctName) {
-            //2. call function to assemble end-point URL
-            fnGetEndPntURL(ctName);
-        }
-        else{
-            //TODO: 3. return error message to user
-        }
+    function fnQueryWeatherEndPnt(endPntURL){
+        //querying end point to obtain Lon and Lat
+        //the response will passed into another function that will 
+        //query the API again for one-call data
+        $.ajax({
+            url: endPntURL,
+            method: "GET"
+        }).then(fnQueryWeatherEndPntOneCall); //passing function by reference. it is only evaluated once the ajax call is completed/success
     }
 
-    function fnGetEndPntURL(ctName, epType){
+    function fnQueryWeatherEndPntOneCall(response){
+        //1st getting API End Point for One Call option
+        let oneCallEndPointURL = fnGetEndPntURLOneCall(response.coord);
+        console.log("One Call URL ", oneCallEndPointURL);
+    }
+
+
+
+    function fnGetEndPntURL(ctName){
         //function returns current or one call end point for the city requested by user.
-        let endPntURL = "";
-        switch(epType) {
-            case "current":
-                //http://api.openweathermap.org/data/2.5/weather?q=London&appid=e936a49dc35d504b97acf268c22c80da
-                endPntURL = "https://api.openweathermap.org/data/2.5/weather?q=" + ctName + "&appid=" + objEndPntSet.appid;
-                break;
-            case "onecall":
-                //1. obtain lat and lon coords for the city. (onecall feature only works with lon/lat)
-                let objLatLon = fnGetLonLat(ctName); //returns an obj "coord": {"lon": -0.13, "lat": 51.51}
-                let strExclude = "";
-                //joining exclude criteria if exclude.legth > 1
-                if (objEndPntSet.exclude.length > 1) {
-                    strExclude = objEndPntSet.exclude.join(",");
-                }
-                else{
-                    strExclude = objEndPntSet.exclude[0];
-                }
-                //https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&appid=e936a49dc35d504b97acf268c22c80da
-                endPntURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + objLatLon.coord.lat + "&lon=" + objLatLon.coord.lon + "&exclude=" + strExclude + "&appid=" + objEndPntSet.appid;
-        }
-        console.log(endPntURL);
+        //http://api.openweathermap.org/data/2.5/weather?q=London&appid=e936a49dc35d504b97acf268c22c80da
+        let endPntURL = "https://api.openweathermap.org/data/2.5/weather?q=" + ctName + "&appid=" + objEndPntSet.appid;
         return endPntURL;
     }
 
-    function fnGetLonLat(ctName){
-
+    function fnGetEndPntURLOneCall(objCoord){
+        //function returns current or one call end point for the city requested by user.
+        let endPntURL = "";
+        let strExclude = "";
+        //joining exclude criteria if exclude.legth > 1
+        if (objEndPntSet.exclude.length > 1) {
+            strExclude = objEndPntSet.exclude.join(",");
+        }
+        else{
+            strExclude = objEndPntSet.exclude[0];
+        }
+        //https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&appid=e936a49dc35d504b97acf268c22c80da
+        endPntURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + objCoord.lat + "&lon=" + objCoord.lon + "&exclude=" + strExclude + "&appid=" + objEndPntSet.appid;
+        return endPntURL;
     }
 });
