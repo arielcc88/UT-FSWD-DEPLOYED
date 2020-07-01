e137854dec61bfd0e1874aceedd7ab5682c849eb
Ariel Cuesta <arielcc88@gmail.com>
2020-06-30 22:48:01 -0500

    questions have been created. Answers are being captured correctly.

--- readme_gen_node/Develop/index.js
+++ readme_gen_node/Develop/index.js
@@ -6,9 +6,14 @@ const inquirer = require("inquirer");
 const questions = [
     //project title question object
     { type: "input", name: "prjTitle", message: "Input Project Title:", validate: function (usrInput) { if (!usrInput) { return "Project Title is required."; } return true; } },
-    { type: "editor", name: "prjDesc", message: "Input Project Description:", validate: function (usrInput) { if (!usrInput) { return "Project Description is required."; } return true; } }
-    { type: "editor", name: "prjInstall", message: "Indicate Installation Instructions:", validate: function (usrInput) { if (!usrInput) { return "Install Instructions are required."; } return true; } }
-    { type: "editor", name: "prjUsage", message: "Add Usage Information:", validate: function (usrInput) { if (!usrInput) { return "Project Description is required."; } return true; } }
+    { type: "editor", name: "prjDesc", message: "Input Project Description:", validate: function (usrInput) { if (!usrInput) { return "Project Description is required."; } return true; } },
+    { type: "editor", name: "prjInstall", message: "Indicate Installation Instructions:", validate: function (usrInput) { if (!usrInput) { return "Install Instructions are required."; } return true; } },
+    { type: "input", name: "prjUsage", message: "Add Usage Information:", validate: function (usrInput) { if (!usrInput) { return "Usage Information is required."; } return true; } },
+    { type: "input", name: "prjContr", message: "Input Contribution Guidelines:", validate: function (usrInput) { if (!usrInput) { return "Contribution Guidelines are required."; } return true; } },
+    { type: "input", name: "prjTestInst", message: "Indicate Test Instructions:", validate: function (usrInput) { if (!usrInput) { return "Test Instructions are required."; } return true; } },
+    { type: "list", name: "prjLicense", message: "Indicate License for this Project:", choices: ["PD", "CCO", "MIT", "Apache", "MPL", "GPL", "AGPL", "JRL", "AFPL", "Proprietary Software"], default: "MIT"},
+    { type: "input", name: "prjGitUsr", message: "Enter GitHub username for Contact Section:", validate: function (usrInput) { if (!usrInput) { return "GitHub username is required."; } return true; } },
+    { type: "input", name: "prjDevEmail", message: "Enter email address for Contact Section:", validate: function (usrInput) { if (!usrInput) { return "Email is required."; } return true; } },
 
 ];
 
